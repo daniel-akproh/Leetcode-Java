e9b78c1c2709cf4bcdfe1de7fc9d12f19fd0e705
Daniel Akproh <daniel.akproh@outlook.com>
2025-01-31 22:41:57 -0500

    Reverse linked list

---
+++ 206. Reverse Linked List/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    // 1->2->3->4->5->6->7->NULL
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+}
